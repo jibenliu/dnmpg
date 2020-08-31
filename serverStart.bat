3779b2476b0c2089c98d61aeaeddba60a86f7922
jibenliu <jibenliu@126.com>
2020-08-31 16:53:00 +0800

    调整配置文件

--- serverStart.bat
+++ serverStart.bat
@@ -1,4 +1,4 @@
 cd /d D:\dnmp\
 net start lanmanserver
-tasklist | find /i "Docker for Windows.exe" && docker-compose up -d || call "C:\Program Files\Docker\Docker\Docker for Windows.exe"
-docker-compose up -d
+tasklist | find /i "Docker for Windows.exe" && docker-compose --compatibility up -d || call "C:\Program Files\Docker\Docker\Docker for Windows.exe"
+docker-compose --compatibility up -d
